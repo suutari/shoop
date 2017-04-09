16e3d127eb13e417cf503c0a8e2a524910f3eed0
Tuomas Suutari <tuomas@nepnep.net>
2017-04-09 16:03:47 +0300

    wip2

--- shuup/utils/money_babel_utils.py
+++ shuup/utils/money_babel_utils.py
@@ -14,9 +14,6 @@ from collections import defaultdict
 import babel.core
 from babel.numbers import format_currency
 
-_precision_cache = None
-_digits_cache = None
-
 
 def get_precision(currency):
     """
@@ -28,31 +25,7 @@ def get_precision(currency):
     :rtype: decimal.Decimal
     :return: Precision value for given currency code
     """
-    global _precision_cache
-    if _precision_cache is None:
-        _precision_cache = _generate_precision_cache()
-    return _precision_cache[currency]
-
-
-def _generate_precision_cache():
-    currency_fractions = babel.core.get_global('currency_fractions')
-    values = {
-        currency: decimal.Decimal('0.1') ** data[0]
-        for (currency, data) in currency_fractions.items()
-    }
-    default = values.pop('DEFAULT')
-    cache = defaultdict(lambda: default)
-    cache.update(values)
-    return cache
-
-
-def _generate_digits_cache():
-    fractions = babel.core.get_global('currency_fractions')
-    values = {currency: data[0] for (currency, data) in fractions.items()}
-    default = values.pop('DEFAULT')
-    cache = defaultdict(lambda: default)
-    cache.update(values)
-    return cache
+    return _get_precision_from_cache(currency)
 
 
 def format_money(value, currency, digits=None, widen=0, locale=None):
@@ -73,10 +46,6 @@ def format_money(value, currency, digits=None, widen=0, locale=None):
     :return: Formatted string
     :rtype: str
     """
-    global _digits_cache
-    if _digits_cache is None:
-        _digits_cache = _generate_digits_cache()
-
     if not locale:
         loc = get_current_babel_locale()
     else:
@@ -87,8 +56,45 @@ def format_money(value, currency, digits=None, widen=0, locale=None):
     # pattern is a formatting string.  Couple examples:
     # '¤#,##0.00', '#,##0.00\xa0¤', '\u200e¤#,##0.00', and '¤#0.00'
 
-    places = (_digits_cache[currency] if digits is None else digits) + widen
+    d = digits if digits is not None else _get_digits_from_cache(currency)
+    places = d + widen
     pattern = pattern.replace(".00", "." + (places * "0"))
 
     return format_currency(value, currency, pattern, loc, currency_digits=False)
 
+
+def _get_digits_from_cache(currency):
+    return _fill_caches_and_set_cache_getters()[0](currency)
+
+
+def _get_precision_from_cache(currency):
+    return _fill_caches_and_set_cache_getters()[1](currency)
+
+
+def _fill_caches_and_set_cache_getters():
+    global _get_digits_from_cache
+    global _get_precision_from_cache
+
+    (get_digits, get_precision) = _get_digit_and_precision_getters()
+
+    _get_digits_from_cache = get_digits
+    _get_precision_from_cache = get_precision
+
+    return (get_digits, get_precision)
+
+
+def _get_digit_and_precision_getters():
+    fractions = babel.core.get_global('currency_fractions')
+    digits = _make_defaultdict(
+        (currency, data[0])
+        for (currency, data) in fractions.items())
+    precisions = _make_defaultdict(
+        (currency, decimal.Decimal('0.1') ** data[0])
+        for (currency, data) in fractions.items())
+    return (digits.__getitem__, precisions.__getitem__)
+
+
+def _make_defaultdict(iterable, default_key='DEFAULT'):
+    values = {key: value for (key, value) in iterable}
+    default = values.pop(default_key)
+    return defaultdict(lambda: default, values)
